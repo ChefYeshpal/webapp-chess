c25c26328694159c415e54dbce8a65bd5a5dc99d
valkarie <ChefYeshpal>
2025-09-24 18:07:10 +0530

    added parameters

---
+++ stockfish.js
@@ -0,0 +1,35 @@
+// Create a stockfish engine as a webworker
+const stockfish = new Worker('https://cdn.jsdelivr.net/npm/stockfish@16.1.0/dist/stockfish.min.js');
+
+// Flag to track if stockfish is ready
+let stockfishReady = false;
+
+// Listen for stockfish messages
+stockfish.onmessage = function(event) {
+    if (event.data === 'uciok') {
+        stockfishReady = true;
+    }
+};
+
+// Send initial UCI command to start the engine
+stockfish.postMessage('uci');
+
+// Function to get stockfish best move for a given FEN position
+function fetchStockfishMove(fen, callback) {
+    if (!stockfishReady) {
+        console.warn("Stockfish is not ready yet!")
+        return;
+    }
+    stockfish.postMessage('position fen ' + fen);
+    // Can adjust depth for a slower/faster engine
+    stockfish.postMessage('go depth 15'); 
+
+    stockfish.onmessage = function(event) {
+        const line = event.data;
+        // Listen for best move response
+        if (line.startsWith('bestmove')) {
+            const move = line.split(' ')[1];
+            callback(move);
+        }
+    };
+}
